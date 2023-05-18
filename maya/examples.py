5500b76c77d3cc58edc8750c8e79074695624716
mondongo <rguguerra@outlook.com>
2023-05-18 00:30:36 -0600

    a

--- maya/examples.py
+++ maya/examples.py
@@ -67,3 +67,97 @@ for v in vertices:
     vNor.append([nX, nY, nZ])
 
 print(vNor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ======================================================================>
+import   as OpenMaya
+
+# Obtener el objeto MObject de la malla seleccionada
+selList = OpenMaya.MSelectionList()
+OpenMaya.MGlobal.getActiveSelectionList(selList)
+dagPath = OpenMaya.MDagPath()
+selList.getDagPath(0, dagPath)
+meshObject = dagPath.node()
+
+# Crear un objeto MFnMesh a partir del objeto MObject de la malla
+meshFn = OpenMaya.MFnMesh(dagPath)
+
+# Obtener las posiciones de los vértices de la malla
+points = OpenMaya.MPointArray()
+meshFn.getPoints(points, OpenMaya.MSpace.kWorld)
+
+# Iterar a través de los vértices de la malla y obtener sus normales
+normals = OpenMaya.MFloatVectorArray()
+meshFn.getVertexNormals(True, normals, OpenMaya.MSpace.kWorld)
+
+
+# for i in range(normals.length()):
+#     normal = normals[i]
+#     print("Normal del vértice {}: ({}, {}, {})".format(i, normal.x, normal.y, normal.z))
+
+
+# for i in range(points.length()):
+#     point = points[i]
+#     print("Posición del vértice {}: ({}, {}, {})".format(i, point.x, point.y, point.z))
+
+print(normals)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------------->
+import maya.OpenMaya as om
+
+
+mSelectionList = om.MSelectionList()
+om.MGlobal.getActiveSelectionList(mSelectionList)
+
+if mSelectionList.isEmpty():
+    print("No hay objetos seleccionados.")
+else:
+    mDagPath = om.MDagPath()
+    mSelectionList.getDagPath(0, mDagPath)
+    meshObject = mDagPath.node()
+    fnMesh = om.MFnMesh(mDagPath)
+
+
+    meshFn = om.MFnMesh(mDagPath)
+
+    points = om.MPointArray()
+    meshFn.getPoints(points, om.MSpace.kWorld)
+
+    normals = om.MFloatVectorArray()
+    meshFn.getVertexNormals(True, normals, om.MSpace.kWorld)
+
+    for i in range(normals.length()):
+        normal = normals[i]
+        print("Normal del vértice {}: ({}, {}, {})".format(i, normal.x, normal.y, normal.z))    
+    for i in range(points.length()):
+        point = points[i]
+        print("Posición del vértice {}: ({}, {}, {})".format(i, point.x, point.y, point.z))
